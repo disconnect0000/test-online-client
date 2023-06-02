77d2c740b4a9caf01fadec49eee81e604bcfc502
unknown <makemoney76311@gmail.com>
2023-06-02 14:23:34 +0100

    f

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import axios from "axios";
 function App() {
+  function handleReq() {
+    axios.get("http://localhost:3001").then((res) => {
+      console.log(res);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleReq}>Request</button>
     </div>
   );
 }
