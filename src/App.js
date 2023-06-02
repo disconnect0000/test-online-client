35a1c11e46921dfd13d4d4afb120bcb054259af9
unknown <makemoney76311@gmail.com>
2023-06-02 18:17:16 +0100

    f

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import "./App.css";
 import axios from "axios";
+import React from "react";
+import { useState } from "react";
 function App() {
+  const [value, setValue] = useState("");
   function handleReq() {
     axios
       .get("https://test-oonline.onrender.com/test")
       .then((res) => console.log(res.data));
   }
+
+  function getUsers() {
+    axios
+      .get("https://test-oonline.onrender.com/")
+      .then((res) => console.log(res.data));
+  }
+  function importUser() {
+    axios
+      .post("http://localhost:5000", { username: value, password: value })
+      .then((res) => console.log(res.data));
+  }
+
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
   return (
     <div className="App">
       <button onClick={handleReq}>Request</button>
+      <button onClick={getUsers}>Users</button>
+      <input value={value} onChange={handleChange} />
+      <button onClick={importUser}>Import User</button>
     </div>
   );
 }
